9818e7ec18aa864a9990dd009d430a4e64aa40fe
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-04-26 16:03:55 -0400

    Created users table using knex migrations and seeds

---
+++ database/seeds/01-users.js
@@ -0,0 +1,28 @@
+exports.seed = function (knex) {
+	// Deletes ALL existing entries
+	return knex("users")
+		.truncate()
+		.then(function () {
+			// Inserts seed entries
+			return knex("users").insert([
+				{
+					id: 1,
+					username: "devOne",
+					password: "password1",
+					department: "FrontEnd"
+				},
+				{
+					id: 2,
+					username: "devTwo",
+					password: "password2",
+					department: "Backend"
+				},
+				{
+					id: 3,
+					username: "devThree",
+					password: "password3",
+					department: "UX/UI"
+				}
+			]);
+		});
+};
